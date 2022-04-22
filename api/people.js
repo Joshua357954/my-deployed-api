e8556cd3e3a6f0b48a0cb5288d22f6f3d4c935d4
Joshuaboyi <boyijoshua72@gmail.com>
2022-04-22 05:36:30 +0100

    fills

--- api/people.js
+++ api/people.js
@@ -1,13 +1,46 @@
-const express = require("express")
-const router = express.Router()
+// const express = require("express")
+// const router = express.Router()
 
 
 
-router.get('/',(req,res)=>{
+// router.get('/',(req,res)=>{
 
-	res.write("<h1 style='text-align:center; font-family: comic sans ms; color: cornflowerblue'; margin-top:30px>\
-		Welcome to My Varcel Api ..</h1>")
-	res.send()
-})
+// 	res.write("<h1 style='text-align:center; font-family: comic sans ms; color: cornflowerblue'; margin-top:30px>\
+// 		Welcome to My Varcel Api ..</h1>")
+// 	res.send()
+// })
 
-module.exports=router
+// module.exports=router
+
+
+nst express = require("express");
+const router = express.Router();
+
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully (Joshua)",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
