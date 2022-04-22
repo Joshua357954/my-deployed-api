2ec556944e18d82243ac00a680faebbc7fe8a68e
Joshuaboyi <boyijoshua72@gmail.com>
2022-04-22 06:12:56 +0100

    diir

--- api/people.js
+++ api/people.js
@@ -1,41 +1,41 @@
-const express = require("express")
-const router = express.Router()
+// const express = require("express")
+// const router = express.Router()
 
 
 
-router.get('/',(req,res)=>{
+// router.get('/',(req,res)=>{
 
-	res.write("<h1 style='text-align:center; font-family: comic sans ms; color: cornflowerblue'; margin-top:30px>\
-		Welcome to My Varcel Api ..</h1>")
-	res.send()
+// 	res.write("<h1 style='text-align:center; font-family: comic sans ms; color: cornflowerblue'; margin-top:30px>\
+// 		Welcome to My Varcel Api ..</h1>")
+// 	res.send()
 
-	// return res.status(500).send("Server error");
-})
+// 	// return res.status(500).send("Server error");
+// })
 
-module.exports=router
+// module.exports=router
 
 
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// /**
-//  * GET product list.
-//  *
-//  * @return product list | empty.
-//  */
-// router.get("/", async (req, res) => {
-//   try {
-//     res.json({
-//       status: 200,
-//       message: "Get data has successfully (People here ...)",
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).send("Server error");
-//   }
-// });
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully (People here ...)",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
 
-// module.exports = router;
+module.exports = router;
 
 
 
