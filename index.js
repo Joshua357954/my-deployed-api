d6772ecbdbb5646b00333b85922252290ed2a1bf
Joshuaboyi <boyijoshua72@gmail.com>
2022-04-22 05:46:43 +0100

    fomxi

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-const product = require("./api/pk");
 
 const people = require("./api/people");
 
 app.use(express.json({ extended: false }));
-
-app.use("/", product);
 
 app.use("/pe", people);
 
