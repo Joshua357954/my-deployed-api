fa29f7fd2b51e0da2b7cdc964f08777d693d7100
Joshuaboyi <boyijoshua72@gmail.com>
2022-04-22 05:31:04 +0100

    foma

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express');
+// const express = require('express');
 
-const persons=require("./api/people");
+// const persons=require("./api/people");
 
+// const app = express();
+
+// app.use(express.json({ extended: false }));
+
+// app.use("/",persons)
+
+
+// const port = process.env.PORT || 4800;
+
+// app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+
+
+const express = require("express");
 const app = express();
+const people = require("./api/people");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/people",persons)
+app.use("/", people);
 
+const PORT = process.env.PORT || 8080;
 
-const port = process.env.PORT || 4800;
-
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
